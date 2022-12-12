bb8ccd1c67ea671e24c451759ced3082bbe1029e
peanut-lover <jihoson94@gmail.com>
2022-12-12 22:46:04 +0900

    :sparkles: Add HtmlWebpackPlugin

--- webpack-tracking-plugin/poc/src/index.js
+++ webpack-tracking-plugin/poc/src/index.js
@@ -1,10 +1,27 @@
 import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.svg";
 
 function component() {
   const element = document.createElement("div");
 
+
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add('hello')
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
--- webpack-tracking-plugin/poc/webpack.config.js
+++ webpack-tracking-plugin/poc/webpack.config.js
@@ -1,17 +1,34 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const options = {};
 
 module.exports = {
-  entry: "./src/index.js",
-
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+    new WebpackManifestPlugin(options)
+  ],
+  mode: "development",
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
