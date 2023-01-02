96495940229eba4b63e07360ab93c98f44294ed6
peanut-lover <jihoson94@gmail.com>
2023-01-02 22:43:27 +0900

    :lipstick: Add Button Stories

--- design-guide/peanut-lover/src/Button/Button.stories.tsx
+++ design-guide/peanut-lover/src/Button/Button.stories.tsx
@@ -1,24 +1,85 @@
 /** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { Button } from "./Button"; 
+import { Button } from "./Button";
 
 export default {
   title: "components/Button",
-  component: Button, 
+  component: Button,
 } as ComponentMeta<typeof Button>;
 
-const Template = (args: any) =>  <Button {...args}/>
+const Template = (args: any) => <Button {...args} />;
 
-export const Primary: ComponentStory<typeof Button> = Template.bind({})
+export const button: ComponentStory<typeof Button> = Template.bind({});
+button.args = {
+  theme: "primary",
+  children: "버튼",
+};
+button.story = {
+  name: "Default",
+};
 
-Primary.args = {
-  theme: 'primary',
-  children: "Button"
-}
+export const Primary: ComponentStory<typeof Button> = () => (
+  <Button theme="primary">확인</Button>
+);
 
-export const Secondary: ComponentStory<typeof Button> = Template.bind({})
+export const Secondary: ComponentStory<typeof Button> = () => (
+  <Button theme="secondary">나중에 확인</Button>
+);
 
-Secondary.args = {
-  theme: 'secondary',
-  children: "Secondary"
-}
+export const Negative: ComponentStory<typeof Button> = () => (
+  <Button theme="negative"> 취소</Button>
+);
+
+const buttonWrapper = css`
+  .description {
+    margin-bottom: 0.5rem;
+  }
+  & > div + div {
+    margin-top: 2rem;
+  }
+`;
+
+export const sizes = () => {
+  return (
+    <div css={buttonWrapper}>
+      <div>
+        <div className="description">Small</div>
+        <Button theme="secondary" size="small">
+          Button
+        </Button>
+      </div>
+      <div>
+        <div className="description">Medium</div>
+        <Button theme="secondary" size="medium">
+          Button
+        </Button>
+      </div>
+      <div>
+        <div className="description">Big</div>
+        <Button theme="secondary" size="big">
+          Button
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export const customWidth = () => {
+  return (
+    <div css={buttonWrapper}>
+      <div>
+        <div className="description">Custom Size</div>
+        <Button theme="secondary" width={"20rem"} size="big">
+          Custom Size
+        </Button>
+      </div>
+      <div>
+        <div className="description">Full</div>
+        <Button theme="secondary" width={"100%"} size="big">
+          Full Button
+        </Button>
+      </div> 
+    </div>
+  );
+};
--- design-guide/peanut-lover/src/Button/Button.tsx
+++ design-guide/peanut-lover/src/Button/Button.tsx
@@ -1,19 +1,51 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { PropsWithChildren } from "react";
-import { BLUE, BLUE_DARK, BLUE_LIGHT, BLUE_STROKE, WHITE } from "../colors";
+import {
+  BLUE,
+  BLUE_DARK,
+  BLUE_LIGHT,
+  BLUE_STROKE,
+  RED,
+  SALMON,
+  WHITE,
+} from "../colors";
 
 type Props = {
   /** theme는 버튼의 중요도를 나트내는 표시 */
-  theme: "primary" | "secondary";
+  theme: "primary" | "secondary" | "negative";
+  /** 버튼의 너비를 임의로 설정합니다. */
+  width?: string | number;
+  /** 버튼의 사이즈 */
+  size?: "small" | "medium" | "big"
 };
 
 export const Button: React.FC<PropsWithChildren<Props>> = ({
   theme,
+  size = "medium",
   children,
+  width,
 }) => {
-  return <button css={[style, themes[theme]]}>{children}</button>;
+  return <button css={[style, themes[theme], sizes[size], { width }]}>{children}</button>;
 };
+ 
+const sizes = {
+  small: css`
+    height: 1.75rem;
+    font-size: 0.75rem;
+    padding: 0 0.875rem;
+  `,
+  medium: css`
+    height: 2.5rem;
+    font-size: 1rem;
+    padding: 0 1rem;
+  `,
+  big: css`
+    height: 3rem;
+    font-size: 1.125rem;
+    padding: 16px 1.5rem;
+  `,
+}; 
 
 const themes = {
   primary: css`
@@ -43,17 +75,28 @@ const themes = {
       background: ${BLUE_STROKE};
     }
   `,
+  negative: css`
+    background: ${SALMON};
+    color: ${RED};
+    &:active {
+      opacity: 0.5;
+    }
+    &:disabled {
+      opacity: 0.5;
+    }
+  `,
 };
 
 const style = css`
   display: flex;
+  justify-content: center;
   align-items: center;
   outline: none;
   border: none;
   box-sizing: border-box;
   height: 2rem;
   font-size: 0.875rem;
-  padding: 0.5rem 1rem;
+  padding: 16px;
   color: white;
   border-radius: 0.25rem;
   line-height: 1;
